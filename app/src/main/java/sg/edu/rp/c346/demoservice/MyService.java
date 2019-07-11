e8c0e8457f551813e1498597bc3035fb5e9a49cf
17010475 <17010475@myrp.edu.sg>
2019-07-11 23:16:05 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/demoservice/MyService.java
@@ -0,0 +1,38 @@
+package sg.edu.rp.c346.demoservice;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+
+public class MyService extends Service {
+
+    boolean started;
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        if (started == false){
+            started = true;
+            Log.d("Service", "Service started");
+        } else {
+            Log.d("Service", "Service is still running");
+        }
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+}
